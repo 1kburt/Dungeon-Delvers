9dbd06d392bcf56d743b33aa2c2cfea78848ad24
1kburt <1kburt@A6-WSD1-B360>
2024-05-22 14:09:51 +1200

    Add project files.

---
+++ Shoot Out Game MOO ICT/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Shoot_Out_Game_MOO_ICT
+{
+    partial class DungeonDelvers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DungeonDelvers));
+            this.label1 = new System.Windows.Forms.Label();
+            this.healthBar = new System.Windows.Forms.ProgressBar();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            this.basicDoor = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.basicDoor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(6, 626);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Health: ";
+            // 
+            // healthBar
+            // 
+            this.healthBar.Location = new System.Drawing.Point(88, 626);
+            this.healthBar.Name = "healthBar";
+            this.healthBar.Size = new System.Drawing.Size(187, 23);
+            this.healthBar.TabIndex = 1;
+            this.healthBar.Value = 100;
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Enabled = true;
+            this.GameTimer.Interval = 20;
+            this.GameTimer.Tick += new System.EventHandler(this.MainTimerEvent);
+            // 
+            // basicDoor
+            // 
+            this.basicDoor.Image = global::Shoot_Out_Game_MOO_ICT.Properties.Resources.castledoors;
+            this.basicDoor.InitialImage = ((System.Drawing.Image)(resources.GetObject("basicDoor.InitialImage")));
+            this.basicDoor.Location = new System.Drawing.Point(859, 272);
+            this.basicDoor.Name = "basicDoor";
+            this.basicDoor.Size = new System.Drawing.Size(65, 100);
+            this.basicDoor.TabIndex = 3;
+            this.basicDoor.TabStop = false;
+            // 
+            // player
+            // 
+            this.player.Image = global::Shoot_Out_Game_MOO_ICT.Properties.Resources.up;
+            this.player.Location = new System.Drawing.Point(427, 471);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(71, 100);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.player.TabIndex = 2;
+            this.player.TabStop = false;
+            // 
+            // DungeonDelvers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(924, 661);
+            this.Controls.Add(this.basicDoor);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.healthBar);
+            this.Controls.Add(this.label1);
+            this.Name = "DungeonDelvers";
+            this.Text = "`";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.basicDoor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar healthBar;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer GameTimer;
+        private System.Windows.Forms.PictureBox basicDoor;
+    }
+}
+
---
+++ Shoot Out Game MOO ICT/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shoot_Out_Game_MOO_ICT
+{
+    public partial class DungeonDelvers : Form
+    {
+
+        bool goLeft, goRight, goUp, goDown, gameOver;
+        string facing = "up";
+        int playerHealth = 100;
+        int speed = 10;
+        Random randNum = new Random();
+
+
+
+        public DungeonDelvers()
+        {
+            InitializeComponent();
+            RestartGame();
+        }
+
+        private void MainTimerEvent(object sender, EventArgs e)
+        {
+            if (playerHealth > 1)
+            {
+                healthBar.Value = playerHealth;
+            }
+            else
+            {
+                gameOver = true;
+                player.Image = Properties.Resources.dead;
+                GameTimer.Stop();
+            }
+
+            if (goLeft == true && player.Left > 0)
+            {
+                player.Left -= speed;
+            }
+            if (goRight == true && player.Left + player.Width < this.ClientSize.Width)
+            {
+                player.Left += speed;
+            }
+            if (goUp == true && player.Top > 45)
+            {
+                player.Top -= speed;
+            }
+            if (goDown == true && player.Top + player.Height < this.ClientSize.Height)
+            {
+                player.Top += speed;
+            }
+
+
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && (string)x.Tag == "ammo")
+                {
+                    if (player.Bounds.IntersectsWith(x.Bounds))
+                    {
+                        this.Controls.Remove(x);
+                        ((PictureBox)x).Dispose();
+
+                    }
+                }
+
+
+                foreach (Control j in this.Controls)
+                {
+                    if (j is PictureBox && (string)j.Tag == "bullet" && x is PictureBox && (string)x.Tag == "zombie" )
+                    {
+                        if (x.Bounds.IntersectsWith(j.Bounds))
+                        {
+
+                            this.Controls.Remove(j);
+                            ((PictureBox)j).Dispose();
+                            this.Controls.Remove(x);
+                            ((PictureBox)x).Dispose();
+                        }
+                    }
+                }
+
+
+            }
+
+
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+
+            if (gameOver == true)
+            {
+                return;
+            }
+
+            if (e.KeyCode == Keys.A)
+            {
+                goLeft = true;
+                facing = "left";
+                player.Image = Properties.Resources.left;
+            }
+
+            if (e.KeyCode == Keys.D)
+            {
+                goRight = true;
+                facing = "right";
+                player.Image = Properties.Resources.right;
+            }
+
+            if (e.KeyCode == Keys.W)
+            {
+                goUp = true;
+                facing = "up";
+                player.Image = Properties.Resources.up;
+            }
+
+            if (e.KeyCode == Keys.S)
+            {
+                goDown = true;
+                facing = "down";
+                player.Image = Properties.Resources.down;
+            }
+
+
+
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.A)
+            {
+                goLeft = false;
+            }
+
+            if (e.KeyCode == Keys.D)
+            {
+                goRight = false;
+            }
+
+            if (e.KeyCode == Keys.W)
+            {
+                goUp = false;
+            }
+
+            if (e.KeyCode == Keys.S)
+            {
+                goDown = false;
+            }
+
+            if (e.KeyCode == Keys.Space && gameOver == false)
+            {
+                ShootBullet(facing);
+
+
+            }
+
+            if (e.KeyCode == Keys.Enter && gameOver == true)
+            {
+                RestartGame();
+            }
+
+        }
+
+        private void ShootBullet(string direction)
+        {
+            Bullet shootBullet = new Bullet();
+            shootBullet.direction = direction;
+            shootBullet.bulletLeft = player.Left + (player.Width / 2);
+            shootBullet.bulletTop = player.Top + (player.Height / 2);
+            shootBullet.MakeBullet(this);
+        }
+
+
+        private void RestartGame() //edit this
+        {
+            player.Image = Properties.Resources.up;
+
+            goUp = false;
+            goDown = false;
+            goLeft = false;
+            goRight = false;
+            gameOver = false;
+
+            playerHealth = 100;
+
+            GameTimer.Start();
+        }
+
+
+        private void LoadRoom()
+        {
+            if (((PictureBox)player).Bounds.IntersectsWith(basicDoor.Bounds))
+            {
+                if(basicDoor.Location.X < 50) //if door on left
+                {
+                    player.Top = 127;
+
+                }
+                else if (basicDoor.Location.X > 50) //if door on right
+                {
+
+                }
+                else if (basicDoor.Location.Y > 50) //if door on top
+                {
+
+                }
+                else //if door on bottom
+                {
+
+                }
+            }
+        }
+    }
+}
